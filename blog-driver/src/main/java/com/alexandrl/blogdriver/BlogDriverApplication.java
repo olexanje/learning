83ad5d6b5fcedb53a08bf45e417db7d93b50b2dc
Alexandr Luzyanin <alexandr.luzyanin@gmail.com>
2022-02-16 20:57:37 +0300

    init commit

---
+++ blog-driver/src/main/java/com/alexandrl/blogdriver/BlogDriverApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrl.blogdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogDriverApplication.class, args);
+	}
+
+}
---
+++ blog-driver/src/test/java/com/alexandrl/blogdriver/BlogDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrl.blogdriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
